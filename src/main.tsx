caea6c7e12e27b817183447c1c2dd2846dd9a51a
natangte <natanaelfranklingt@gmail.com>
2026-09-03 08:40:54 -0300

    Modificação na exportação do App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Qualquercoisa from './App'
+import {App} from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Qualquercoisa />
+    <App />
   </StrictMode>,
 )
