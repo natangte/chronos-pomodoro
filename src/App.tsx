86ed1c23bd1d192c2bda1e210dd54a289aa35f7c
natangte <natanaelfranklingt@gmail.com>
2026-09-10 09:51:05 -0300

    Criando o componente de logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { Heading } from './components/Heading';
+import {Container} from './components/Container'
+import { Logo } from './components/Logo';
 
 import './styles/theme.css'
 import './styles/global.css'
 
-import { Heading } from './components/Heading';
-import {Container} from './components/Container'
 export function App(){
   
   return(
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo></Logo>
       </Container>
       <Container>
         <Heading>MENU</Heading>
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import { TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
+
+export function Logo(){
+    return <div className={styles.logo}>
+        <a className={styles.logoLink}href='#'>
+            <TimerIcon></TimerIcon>
+            <span>Chronos</span>
+        </a>
+    </div>;
+}
